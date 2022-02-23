8431aa2e2b1c606bbff80f16b7454289f21b7c1a
cmarro3 <christophermarroquin352@gmail.com>
2022-02-23 16:00:56 -0600

    2/23/22 pushing all up to date files.

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class QuestionController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('What a bewitching controller we have conjured!');
+    }
+    /**
+     * @Route("/questions/how-to-tie-my-shoes-with-magic")
+     */
+    public function show()
+    {
+        return new Response('Future page to show a question!');
+    }
+}
